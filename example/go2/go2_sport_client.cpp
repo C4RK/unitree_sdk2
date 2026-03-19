b28f5495bff277423001e83785ffe8a51ec3258a
Kun <studium350@gmail.com>
2026-03-19 01:02:39 +0100

    运动

--- example/go2/go2_sport_client.cpp
+++ example/go2/go2_sport_client.cpp
@@ -4,28 +4,28 @@
 
 #include <cmath>
 
-#include <unitree/robot/go2/sport/sport_client.hpp>
+#include <unitree/robot/go2/sport/sport_client.hpp>//内部封装了算法
 #include <unitree/robot/channel/channel_subscriber.hpp>
-#include <unitree/idl/go2/SportModeState_.hpp>
+#include <unitree/idl/go2/SportModeState_.hpp>//状态反馈单。返回的是高层语义信息
 
-#define TOPIC_HIGHSTATE "rt/sportmodestate"
+#define TOPIC_HIGHSTATE "rt/sportmodestate"//DDS话题，程序会订阅这个话题，实时获取机器人的运行状态。
 
 using namespace unitree::common;
 
 enum test_mode
 {
   /*---Basic motion---*/
-  normal_stand,
-  balance_stand,
-  velocity_move,
-  stand_down,
-  stand_up,
-  damp,
-  recovery_stand,
+  normal_stand,//普通的四足站立
+  balance_stand,//平衡站立
+  velocity_move,//速度移动
+  stand_down,//趴下
+  stand_up,//起立
+  damp,//阻尼模式
+  recovery_stand,//自恢复起立
   /*---Special motion ---*/
-  sit,
+  sit,//坐
   rise_sit,
-  stop_move = 99
+  stop_move = 99//立即停止当前的移动指令，锁定在当前位置
 };
 
 const int TEST_MODE = stand_down;
@@ -128,13 +128,13 @@ public:
     // std::cout << "IMU rpy: " << state.imu_state().rpy()[0] << ", " << state.imu_state().rpy()[1] << ", " << state.imu_state().rpy()[2] << std::endl;
   };
 
-  unitree_go::msg::dds_::SportModeState_ state;
-  unitree::robot::go2::SportClient sport_client;
-  unitree::robot::ChannelSubscriberPtr<unitree_go::msg::dds_::SportModeState_> suber;
+  unitree_go::msg::dds_::SportModeState_ state;//一个结构体，用来实时存储从机器人传回来的最新状态
+  unitree::robot::go2::SportClient sport_client;//指令发送器，通过它，可以调用Move等API
+  unitree::robot::ChannelSubscriberPtr<unitree_go::msg::dds_::SportModeState_> suber;//订阅者指针。它负责在后台默默地接收机器人的状态包
 
   double px0, py0, yaw0; // 初始状态的位置和偏航
-  double ct = 0;         // 运行时间
-  int flag = 0;          // 特殊动作执行标志
+  double ct = 0;         // 累积运行时间
+  int flag = 0;          // 特殊动作执行标志，状态切换开关。
   float dt = 0.005;      // 控制步长0.001~0.01
 };
 
